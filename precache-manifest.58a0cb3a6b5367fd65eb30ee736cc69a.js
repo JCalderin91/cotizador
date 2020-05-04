4885fa56881207e27fb20debe63978f545c7ea0b
Jesus Calderin <jesuscalderin91@gmail.com>
2020-05-04 17:18:22 -0400

    deploy

---
+++ precache-manifest.58a0cb3a6b5367fd65eb30ee736cc69a.js
@@ -0,0 +1,286 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5581dd8280d23517215b",
+    "url": "/cotizador/css/app.d4896dd3.css"
+  },
+  {
+    "revision": "c51ad29ab9d9a528f5517116b3330c13",
+    "url": "/cotizador/css/bootstrap.min.css"
+  },
+  {
+    "revision": "3f7455874e8a99082ef0",
+    "url": "/cotizador/css/calcular.6f1ce6bf.css"
+  },
+  {
+    "revision": "0e12dd29f690c5c00d77",
+    "url": "/cotizador/css/chunk-09320e47.6f1ce6bf.css"
+  },
+  {
+    "revision": "0068916b2f467e423e27",
+    "url": "/cotizador/css/chunk-26ed1d4b.587581ae.css"
+  },
+  {
+    "revision": "ce18fc5a04a20c6086dc",
+    "url": "/cotizador/css/chunk-ce41d290.cba890cc.css"
+  },
+  {
+    "revision": "d93abc659188d237c44a",
+    "url": "/cotizador/css/chunk-f54a8c6c.6f1ce6bf.css"
+  },
+  {
+    "revision": "05df168c15c1c916da67",
+    "url": "/cotizador/css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "8b9707f175b898d7194ef106eba0ddf7",
+    "url": "/cotizador/img/E-tienda.8b9707f1.png"
+  },
+  {
+    "revision": "11f2015879562b9f354cab6d9b1a912f",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 2.48.44 PM.11f20158.jpeg"
+  },
+  {
+    "revision": "11f2015879562b9f354cab6d9b1a912f",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 3.13.25 PM.11f20158.jpeg"
+  },
+  {
+    "revision": "11f2015879562b9f354cab6d9b1a912f",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 3.53.29 PM.11f20158.jpeg"
+  },
+  {
+    "revision": "bc94278e734423cdae631c8ff2f8ad80",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 3.54.23 PM.bc94278e.jpeg"
+  },
+  {
+    "revision": "34e472ec4b2b171ce0e1a497a461b1d3",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 3.55.42 PM.34e472ec.jpeg"
+  },
+  {
+    "revision": "c6b9a18ea3cd8ccf01a78c094258698e",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 3.56.06 PM.c6b9a18e.jpeg"
+  },
+  {
+    "revision": "f996f7483d58972d206fbccaa00b6f60",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 3.57.50 PM.f996f748.jpeg"
+  },
+  {
+    "revision": "005a8e31dc079b0d4548ef414a1a357a",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 4.34.59 PM.005a8e31.jpeg"
+  },
+  {
+    "revision": "514cd969e1bec8a9f6022a9ac7248638",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 4.35.34 PM.514cd969.jpeg"
+  },
+  {
+    "revision": "3673b61b130821fbe29364069f867d8f",
+    "url": "/cotizador/img/WhatsApp Image 2020-05-02 at 4.37.38 PM.3673b61b.jpeg"
+  },
+  {
+    "revision": "045e9b7e6f60a081ecf9c5123b12e81d",
+    "url": "/cotizador/img/a-medida.045e9b7e.png"
+  },
+  {
+    "revision": "6dcbd47b0e95b832a120b44cfad3e0ec",
+    "url": "/cotizador/img/background.6dcbd47b.png"
+  },
+  {
+    "revision": "d5606d50dca1f8e08c95025974eb2db1",
+    "url": "/cotizador/img/boceto.d5606d50.png"
+  },
+  {
+    "revision": "454a93d518ef9a62e733d711c06efddc",
+    "url": "/cotizador/img/buscador.454a93d5.png"
+  },
+  {
+    "revision": "b7075f88a286bdfd74cb51341fe017fa",
+    "url": "/cotizador/img/diseño.b7075f88.png"
+  },
+  {
+    "revision": "e520b1fa8dbeff87d68429d9a295ea7a",
+    "url": "/cotizador/img/erp.e520b1fa.png"
+  },
+  {
+    "revision": "ca82d4a9e4e7138ff459116df4e781bf",
+    "url": "/cotizador/img/grande.ca82d4a9.png"
+  },
+  {
+    "revision": "60cdc6c5dc7c2739b86c1be62cf4b1d4",
+    "url": "/cotizador/img/idioma.60cdc6c5.png"
+  },
+  {
+    "revision": "bfafc6ed179686798eee14fbc94e05a6",
+    "url": "/cotizador/img/login.bfafc6ed.png"
+  },
+  {
+    "revision": "018942ab382611c9178436a82388e915",
+    "url": "/cotizador/img/logo-black.018942ab.png"
+  },
+  {
+    "revision": "de8d195ee486f8f8162ea836fa873aa3",
+    "url": "/cotizador/img/mediana.de8d195e.png"
+  },
+  {
+    "revision": "85148e9d4bef7301a2258463ee508dcf",
+    "url": "/cotizador/img/no-buscador.85148e9d.png"
+  },
+  {
+    "revision": "9e61af36fa65d5e8de7dcb2a99b99b86",
+    "url": "/cotizador/img/no-diseño.9e61af36.jpeg"
+  },
+  {
+    "revision": "f998f3811662177fa9976f9334ea271d",
+    "url": "/cotizador/img/no-diseño.f998f381.png"
+  },
+  {
+    "revision": "1db4ae32eeaab979b763f9867ee1ef26",
+    "url": "/cotizador/img/no-erp.1db4ae32.png"
+  },
+  {
+    "revision": "55be8ecfea72e7cc07446e19e21e317c",
+    "url": "/cotizador/img/no-idioma.55be8ecf.png"
+  },
+  {
+    "revision": "0f8a683b87cc9feb294696b643e38f48",
+    "url": "/cotizador/img/no-login.0f8a683b.png"
+  },
+  {
+    "revision": "aff326d4222cd1616f8173fe3078952b",
+    "url": "/cotizador/img/no-pagos.aff326d4.png"
+  },
+  {
+    "revision": "5ccc1528974a03facdf619b893e592b8",
+    "url": "/cotizador/img/no-se.5ccc1528.png"
+  },
+  {
+    "revision": "1a0c137729a9acf168dabe4b396cc755",
+    "url": "/cotizador/img/no-seo.1a0c1377.png"
+  },
+  {
+    "revision": "9ae78cd28eed390de8d9a71c13098b5b",
+    "url": "/cotizador/img/no.9ae78cd2.png"
+  },
+  {
+    "revision": "4624e9e947b9fb03ebebc9854949823f",
+    "url": "/cotizador/img/nose-buscador.4624e9e9.png"
+  },
+  {
+    "revision": "12658d06594e030aeba9bf95d3d1b733",
+    "url": "/cotizador/img/nose-erp.12658d06.png"
+  },
+  {
+    "revision": "32ac789c90cef886c7673f60ec0a10ca",
+    "url": "/cotizador/img/nose-idioma.32ac789c.png"
+  },
+  {
+    "revision": "c55ba9187f2db2383885329f551c3eb5",
+    "url": "/cotizador/img/nose-login.c55ba918.png"
+  },
+  {
+    "revision": "4d92f60de0da9137d9c667555df143f6",
+    "url": "/cotizador/img/nose-pagos.4d92f60d.png"
+  },
+  {
+    "revision": "e8152c8a19654a48d5b6d62ed704af43",
+    "url": "/cotizador/img/nose-seo.e8152c8a.png"
+  },
+  {
+    "revision": "f61027a84e7770d348269e69ee550b84",
+    "url": "/cotizador/img/nose-tamanio.f61027a8.png"
+  },
+  {
+    "revision": "c3ac615e4d4ab5ece5f914d10c725110",
+    "url": "/cotizador/img/pagos.c3ac615e.png"
+  },
+  {
+    "revision": "880ad251afa3dd63f20484f84d7ff527",
+    "url": "/cotizador/img/pequenia.880ad251.png"
+  },
+  {
+    "revision": "a68bc3cc2e8bb834dfad3c3ee81cba84",
+    "url": "/cotizador/img/plantilla.a68bc3cc.png"
+  },
+  {
+    "revision": "14102f8f0bff0794e4e8e818595d45f0",
+    "url": "/cotizador/img/seo.14102f8f.png"
+  },
+  {
+    "revision": "40a4c265cec665923299c71e4d3101aa",
+    "url": "/cotizador/img/si.40a4c265.png"
+  },
+  {
+    "revision": "4cc08f9e1493c5b13d16a2b43e97cfb0",
+    "url": "/cotizador/img/solo-idea.4cc08f9e.png"
+  },
+  {
+    "revision": "b8a575f0d950f33250d5d7aa88a185a6",
+    "url": "/cotizador/img/tiktok.b8a575f0.png"
+  },
+  {
+    "revision": "21bfbb0e5344fbd94930936dfad3c70f",
+    "url": "/cotizador/img/web-a-medida.21bfbb0e.png"
+  },
+  {
+    "revision": "f325882768ac13f839d120a0199f7a00",
+    "url": "/cotizador/img/web-a-medida.f3258827.jpeg"
+  },
+  {
+    "revision": "25db5faac1897c527a730f5663a2e21f",
+    "url": "/cotizador/img/web-blog.25db5faa.png"
+  },
+  {
+    "revision": "3c5e660b1c0ac510070a4e5dc33d2eac",
+    "url": "/cotizador/img/web-blog.3c5e660b.jpeg"
+  },
+  {
+    "revision": "41a6d7b7b122af5fab572680cf586cda",
+    "url": "/cotizador/img/web-en-desarrollo.41a6d7b7.png"
+  },
+  {
+    "revision": "1c43c7029cf75e3024c131ba8b908150",
+    "url": "/cotizador/img/web-lanzada.1c43c702.png"
+  },
+  {
+    "revision": "80389af61a4ad4199e7c2b0c60129a69",
+    "url": "/cotizador/img/web-profecional.80389af6.jpeg"
+  },
+  {
+    "revision": "5b07620a631e8d905558b8b651e81eaf",
+    "url": "/cotizador/index.html"
+  },
+  {
+    "revision": "5581dd8280d23517215b",
+    "url": "/cotizador/js/app.afb595a3.js"
+  },
+  {
+    "revision": "3f7455874e8a99082ef0",
+    "url": "/cotizador/js/calcular.87b6ee9c.js"
+  },
+  {
+    "revision": "0e12dd29f690c5c00d77",
+    "url": "/cotizador/js/chunk-09320e47.328c42ac.js"
+  },
+  {
+    "revision": "0068916b2f467e423e27",
+    "url": "/cotizador/js/chunk-26ed1d4b.18aa58c5.js"
+  },
+  {
+    "revision": "ce18fc5a04a20c6086dc",
+    "url": "/cotizador/js/chunk-ce41d290.dfe96415.js"
+  },
+  {
+    "revision": "d93abc659188d237c44a",
+    "url": "/cotizador/js/chunk-f54a8c6c.33478619.js"
+  },
+  {
+    "revision": "05df168c15c1c916da67",
+    "url": "/cotizador/js/chunk-vendors.15f2002a.js"
+  },
+  {
+    "revision": "d06ac750bd2c2e6615ae7a409aae281e",
+    "url": "/cotizador/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cotizador/robots.txt"
+  }
+]);
